066d58f9476ea786f0c5e98b9465afce6aaa0413
Siddarth Satish <sgsatish@utexas.edu>
2022-03-31 14:13:41 -0500

    added wrong place feature and cosmetic change

--- wordle.py
+++ wordle.py
@@ -23,11 +23,12 @@ if __name__=='__main__':
     print(letters)
     non_letters=[]
     correct=[]
+    place=[]
+    guess=["_", "_", "_", "_","_"]
     while turns <=6:
         print(turns)
         word = input("--> ")
 
-        guess=""
         if not re.match("^[a-z]*$",word) or not len(word)==5 or word not in content:
             print("Error! Word must be a-z, 5 characters long, and a valid word")
             print("---------------------------------------")
@@ -36,24 +37,30 @@ if __name__=='__main__':
             i=0
             while i < 5:
                 if word[i] == ans[i]:
-                    guess+=word[i].upper()
-                    correct.append(word[i].upper())
-                    letters[word[i]]-=1
+                    if guess[i] == "_":
+                        guess[i]=word[i].upper()
+                        #correct.append(word[i].upper())
+                        letters[word[i]]-=1
+                        if word[i] in correct and letters[word[i]] == 0:
+                            correct.remove(word[i])
                 elif word[i] in ans:
-                    if word[i].upper() in correct or letters[word[i]]>0:
-                        guess+=word[i]
+                    if word[i] not in correct and letters[word[i]]>0:
+                        correct+=word[i]
+                    elif word[i] in correct and letters[word[i]]==0:
+                        correct = [y for y in correct if y != word[i]]
                     else:
-                        guess+="_"
+                        pass
                 elif word[i] not in ans:
-                    guess+="_"
+                    #guess+="_"
                     if word[i] not in non_letters:
                         non_letters.append(word[i])
                 i+=1
 
-            print(guess)
+            print(*guess)
             print("These letters are not there: ", non_letters)
+            print("These letters are in the wrong place:", correct )
             print("---------------------------------------")
-            if guess == ans.upper():
+            if '_' not in guess:
                 print("CONGRATS U GOT IT!!")
                 break
             turns+=1
